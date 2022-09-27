08ff4a0f137ae4bd1720ecf09b38b90086f1c1cb
Martin <94791875+PomeloPack@users.noreply.github.com>
2022-09-27 15:51:05 +0200

    Add files via upload

---
+++ Day_6_Python_Function_&_Karel_the_robot.py/6_0_Functios.py
@@ -0,0 +1,26 @@
+# built in functions
+
+print("Hello")
+
+len_num = len("hello")
+print(len_num)
+
+# our own function 
+# using def like definition of function
+
+def  my_function():
+    print("This is our first function")
+    print("It's cool?")
+
+# this is how our function write into the console
+my_function()
+
+#defining function example
+
+"""
+def  my_function():
+    # do this
+    # then do this
+    # finally do this
+"""
+
