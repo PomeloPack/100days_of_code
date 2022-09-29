462627b44dd109e6ac777c687975151af329cbfb
Martin <94791875+PomeloPack@users.noreply.github.com>
2022-09-29 16:09:13 +0200

    Add files via upload

--- Day_6_Python_Function_&_Karel_the_robot.py/6_0_Functios.py
+++ Day_6_Python_Function_&_Karel_the_robot.py/6_0_Functios.py
@@ -7,20 +7,47 @@ print(len_num)
 
 # our own function 
 # using def like definition of function
-
-def  my_function():
-    print("This is our first function")
-    print("It's cool?")
-
-# this is how our function write into the console
-my_function()
-
 #defining function example
 
-"""
 def  my_function():
-    # do this
-    # then do this
-    # finally do this
-"""
+    print("This is our first function") # do this
+    print("It's cool?") # finally do this
 
+# this is how our function write into the console
+# calling function
+my_function()
+
+# compare between for loops and while loops
+
+# for 
+for item in list_of_items:
+    # do something to each item
+
+for number in range(a, b):
+    print(number)
+
+# while
+while something_is_true:
+    # do something repeatedly
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+number_of_jumps = 6
+while number_of_jumps > 0:
+    jump()
+    # -=1 is important because when loops come to the end number_of_jumps -= 1 means that our input 6 is now 5
+    # when output will be 0 loops is False so our condition isn't work
+    number_of_jumps -= 1
