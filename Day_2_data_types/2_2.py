441fb05b3bffe5611b9eb2e0195dc194977c2dc8
Martin <94791875+PomeloPack@users.noreply.github.com>
2022-09-29 11:22:33 +0200

    Add files via upload

---
+++ Day_2_data_types/2_2.py
@@ -0,0 +1,10 @@
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+
+
+float_height = float(height)
+float_weight = float(weight)
+
+BMI = float_weight / ( float_height * float_height)
+print(int(BMI))
